2f2189b838140094558f815382704c0a160b5d8e
Marta Bartosinska <marta@zdalnebabki.pl>
2018-01-31 22:43:36 +0100

    Poprawione
    Zmiana wzoru do obliczania kapitalu końcowego

--- zadania-domowe/zadanie-9/js/index.js
+++ zadania-domowe/zadanie-9/js/index.js
@@ -1,34 +1,36 @@
-function obliczZysk(wplata, iloscLat, okresKapitalizacji, oprocentowanie, podatek){
-  if (podatek) {
-    var oprocentowanieOpodatkowane = oprocentowanie-0.19*oprocentowanie;
-  }
-  else {
-  var oprocentowanieOpodatkowane = oprocentowanie;
-  }
+function obliczZysk(wplata, iloscLat, okresKapitalizacji, oprocentowanie, podatek) {
+
+    if (podatek) {
+        var oprocentowanieOpodatkowane = oprocentowanie - 0.19 * oprocentowanie;
+    } else {
+        var oprocentowanieOpodatkowane = oprocentowanie;
+    }
     console.log("----");
     console.log(okresKapitalizacji);
-  var kapital = Math.pow(wplata*(1+oprocentowanieOpodatkowane/okresKapitalizacji),iloscLat*okresKapitalizacji);
-  return (parseFloat(kapital)).toFixed(2);
+    var kapital = wplata * Math.pow(1 + oprocentowanieOpodatkowane / okresKapitalizacji, iloscLat * okresKapitalizacji);
+    return (parseFloat(kapital)).toFixed(2);
+
 }
 
-function oblicz(){
-  
-  var wplata= parseInt(document.getElementById("wplata").value);
-  var iloscLat= parseInt(document.getElementById("ilosc-lat").value);
-  var okresKapitalizacji= parseInt(document.getElementById("okres-kapitalizacji").value);
-  var oprocentowanie= parseInt(document.getElementById("oprocentowanie").value)/100;
-  var podatek;
-   if (document.getElementById('podatek').checked) {
-            podatek=true;
-        } else {
-            podatek=false;
-        }
-  console.log(wplata);
-  console.log(iloscLat);
-  console.log(okresKapitalizacji);
-  console.log(oprocentowanie);
-  console.log(podatek);
-  var wynik= obliczZysk(wplata, iloscLat, okresKapitalizacji, oprocentowanie, podatek);
-  document.getElementById("wynik").innerHTML = wynik;
-}
+function oblicz() {
 
+    var wplata = parseInt(document.getElementById("wplata").value);
+    var iloscLat = parseInt(document.getElementById("ilosc-lat").value);
+    var okresKapitalizacji = parseInt(document.getElementById("okres-kapitalizacji").value);
+    var oprocentowanie = document.getElementById("oprocentowanie").value / 100;
+    var podatek;
+    if (document.getElementById('podatek').checked) {
+        podatek = true;
+    } else {
+        podatek = false;
+    }
+
+    console.log(wplata);
+    console.log(iloscLat);
+    console.log(okresKapitalizacji);
+    console.log(oprocentowanie);
+    console.log(podatek);
+    var wynik = obliczZysk(wplata, iloscLat, okresKapitalizacji, oprocentowanie, podatek);
+    document.getElementById("wynik").innerHTML = wynik;
+
+}
